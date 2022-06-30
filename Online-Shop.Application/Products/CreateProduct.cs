fcabdb9efefa176ea57831687f598e5c6e8171aa
Zachariasz Soja <zachary3003@gmail.com>
2022-06-30 17:16:50 +0200

    Basic setup and adding product to database

---
+++ Online-Shop.Application/Products/CreateProduct.cs
@@ -0,0 +1,32 @@
+﻿using Online_shop.DataBase;
+using Online_shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Shop.Application.Products
+{
+    public class CreateProduct
+    {
+        private AppDbContext _dbContext;
+
+        public CreateProduct(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Do(string name, string description, decimal value)
+        {
+            _dbContext.Products.Add(new Product
+            {
+                Name = name,
+                Description = description,
+                Value = value
+            });
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Online-Shop.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Online_shop.DataBase;
+using Online_Shop.Application.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Shop.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private AppDbContext _dbContext;
+
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
+
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+
+        public IndexModel(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await new CreateProduct(_dbContext).Do(Product.Name, Product.Description, Product.Value);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
