60e657293a0c72994489acc5d7995e416b72938a
Zachariasz Soja <zachary3003@gmail.com>
2022-07-03 23:02:48 +0200

    Changes in styles and vue components
    Replacing default bootstrap with bulma and creating vue component for managing products

--- Online-Shop.UI/wwwroot/js/admin/product-component.js
+++ Online-Shop.UI/wwwroot/js/admin/product-component.js
@@ -1,12 +1,62 @@
-﻿const app = new Vue({
-    el: '#app',
-    data: {
-        editing: false,
-        loading: false,
-        id: 0,
-        objectIndex: 0,
-        products: [],
-        productModel: { id: 0, name: "Product Name", description: "Product description", value: 0.00 }
+﻿Vue.component("product-manager", {
+    template: 
+        `<div> 
+            <div v-if="!editing">
+                <button class="button" @click="newProduct">Add new product</button>
+                <table class="table">
+                    <tr>
+                        <th>Id</th>
+                        <th>Product</th>
+                        <th>Value</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr v-for="(product, index) in products">
+                        <td>{{product.id}}</td>
+                        <td>{{product.name}}</td>
+                        <td>{{product.value}}</td>
+                        <td><a @click="editProduct(product, index)">Edit</a></td>
+                        <td><a @click="deleteProduct(product.id, index)">Delete</a></td>
+                    </tr>
+                </table>
+            </div>
+
+            <div v-else>
+                <div class="field">
+                    <label class="label">Product Name</label>
+                    <div class="control">
+                        <input v-model="productModel.name" />
+                    </div>
+                </div>
+                
+                <div class="field">
+                    <label class="label">Product Description</label>
+                    <div class="control">
+                        <input v-model="productModel.description" />
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Product Value</label>
+                    <div class="control">
+                        <input v-model="productModel.value" />
+                    </div>
+                </div>
+
+                <button class="button is-success" @click="createProduct" v-if="!productModel.id">Create product</button>
+                <button class="button is-warning" @click="updateProduct" v-else>Update product</button>
+                <button class="button" @click="cancel">Cancel</button>
+            </div>
+        </div>`,
+    data() {
+        return {
+            editing: false,
+            loading: false,
+            id: 0,
+            objectIndex: 0,
+            products: [],
+            productModel: { id: 0, name: "Product Name", description: "Product description", value: 0.00 }
+        }
     },
     mounted() {
         this.getProducts();
@@ -21,7 +71,7 @@
         },
         getProduct(id) {
             this.loading = true;
-            axios.get("/Admin/products/" + id, ).
+            axios.get("/Admin/products/" + id,).
                 then(res => {
                     console.log(res);
                     let product = res.data;
@@ -30,7 +80,8 @@
                         name: product.name,
                         description: product.description,
                         value: product.value,
-                    }}).
+                    }
+                }).
                 catch(error => console.log(error)).
                 then(() => this.loading = false);
         },
@@ -84,4 +135,4 @@
     computed: {
 
     }
-});
+})
